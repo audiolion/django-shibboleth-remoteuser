23d87aee9c6db27c76319a259be99b5bbb9e4ad7
Ryan Castner <castner.rr@gmail.com>
2016-08-17 17:48:42 -0400

    potential name conflict

--- shibboleth/backends.py
+++ shibboleth/backends.py
@@ -35,7 +35,7 @@ class ShibbolethRemoteUserBackend(RemoteUserBackend):
         username = self.clean_username(remote_user)
         field_names = [x.name for x in User._meta.get_fields()]
         shib_user_params = dict([(k, shib_meta[k]) for k in field_names if k in shib_meta])
-        email = shib_user_params.get('email')
+        user_email = shib_user_params.get('email')
         # Note that this could be accomplished in one try-except clause, but
         # instead we use get_or_create when creating unknown users since it has
         # built-in safeguards for multiple threads.
@@ -43,7 +43,7 @@ class ShibbolethRemoteUserBackend(RemoteUserBackend):
             try:
                 user = User.objects.get(rit_username=username)
             except User.DoesNotExist:
-                user, created = User.objects.create_user(rit_username=username, password=None, email=email)
+                user, created = User.objects.create_user(rit_username=username, password=None, email=user_email)
                 if created:
                     """
                     @note: setting password for user needs on initial creation of user instead of after auth.login() of middleware.
